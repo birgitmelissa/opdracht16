997a581044c896554472025706ac4f848c8f7ab6
birgitmelissa <94128573+birgitmelissa@users.noreply.github.com>
2021-12-06 00:29:22 +0100

    Add files via upload

---
+++ opdracht16/script.js
@@ -0,0 +1,31 @@
+console.log("Hello Winc Academy")
+
+let name = 'Melissa';
+console.log(name);
+        
+console.log(4 + 4)
+console.log("4" + "4")
+
+let a = 8;
+let b = a + 9;
+console.log(b)
+
+let d = 8;
+let e = d - 9;
+console.log(e)
+
+let g = 8;
+let h = g * 9;
+console.log(h)
+
+let j = 8;
+let k = j / 9;
+console.log(k)
+
+let m = 8;
+let n = m % 9;
+console.log(n)
+
+let age = 38;
+
+let leeftijd = "38";
